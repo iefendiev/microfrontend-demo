b303175594d0a7f8a71ac07456f494f2d7e9316a
İpek Efendiev <ipekefendiev@gmail.com>
2022-08-13 17:58:07 +0300

    create shared tailwind config

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../../packages/ui/components/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
